f3db319bf25b8746946810961a3c7faebd84aced
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-04 16:24:25 -0500

    🟥 Dev Sidebar

--- client/src/app/(components)/Sidebar.tsx
+++ client/src/app/(components)/Sidebar.tsx
@@ -1,8 +1,31 @@
+"use client"
+
+import { Menu } from "lucide-react"
+
 type Props = {}
 
 const Sidebar = (props: Props) => {
   return (
-    <div>Sidebar</div>
+    <div>
+      {/* TOP LOGO*/}
+      <div className="flex gap-3 justify-between md:justify-normal items-center pt-8">
+        <div>logo</div>
+        <h1 className="font-extrabold text-2xl">EDSTOCK</h1>
+        <button className="md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100" onClick={() => { }}>
+          <Menu className="w-4 h-4" />
+        </button>
+      </div>
+
+      {/* NOTE: LINKS */}
+      <div className="flex-grow mt-8">
+        {/* links here*/}
+      </div>
+
+      {/* FOOTER */}
+      <div>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 WarehouseStock</p>
+      </div>
+    </div>
   )
 }
 
