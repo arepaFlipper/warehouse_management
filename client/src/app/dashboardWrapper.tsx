2ca662b0bc7584efb1277b31c25e77978b6271de
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-05 12:16:00 -0500

    🎨 add the Sidebar & Navbar hide button interaction

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,19 +1,44 @@
+"use client";
+
+import { useEffect } from "react";
 import Navbar from "./(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
+import StoreProvider, { useAppSelector, RootState } from "./redux";
 
 type Props = {
   children: React.ReactNode;
+};
+
+
+const DashboardLayout = ({ children }: Props) => {
+  const isSidebarCollapsed = useAppSelector((state: RootState) => state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state: RootState) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  });
+  return (
+    <StoreProvider>
+      <div className={`${isDarkMode ? "dark" : "light"} light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+        <Sidebar />
+        <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50  ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"} `}>
+          <Navbar />
+          {children}
+        </main>
+      </div>
+    </StoreProvider>
+  );
 }
 
 const DashboardWrapper = ({ children }: Props) => {
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      <Sidebar />
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50`}>
-        <Navbar />
-        {children}
-      </main>
-    </div>
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   )
 }
 
