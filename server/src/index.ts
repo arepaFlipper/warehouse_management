8c25facb0f676d5d606979b3bfcd7cc7cd0203af
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-26 16:08:15 -0500

    🔝 set dev server

---
+++ server/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cors from 'cors';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// NOTE: ROUTES
+app.get("/hello", (req, res) => {
+  res.send("hello world 🌏");
+})
+
+
+// NOTE: SERVER
+const port = process.env.PORT || 3002;
+app.listen(port, (): void => {
+  console.log(`🎙️%cindex.ts:19 - running on port ${port}`, 'font-weight:bold; background:#56a900;color:#fff;'); //DELETEME:
+})
