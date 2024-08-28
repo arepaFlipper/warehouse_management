0452665c80473e5ecebb96409509359cff1f6187
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-28 16:39:20 -0500

    🎱 Display a big bg-gray-500 box

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,8 +1,12 @@
+"use client"
+
 type Props = {}
 
 const Dashboard = (props: Props) => {
   return (
-    <div>Dashboard</div>
+    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <div className="bg-gray-500 row-span-3 xl:row-span-6"></div>
+    </div>
   )
 }
 
