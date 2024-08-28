153543a3829a8042aa6cba40b4a26165987c7564
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-28 16:45:20 -0500

    😞 render grid

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -5,7 +5,13 @@ type Props = {}
 const Dashboard = (props: Props) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
-      <div className="bg-gray-500 row-span-3 xl:row-span-6"></div>
+      <div className="bg-gray-900 row-span-3 xl:row-span-6"></div>
+      <div className="bg-gray-800 row-span-3 xl:row-span-6"></div>
+      <div className="bg-gray-700 row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1"></div>
+      <div className="row-span-3 bg-gray-600"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-500"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-400"></div>
+      <div className="md:row-span-1 xl:row-span-2 bg-gray-300"></div>
     </div>
   )
 }
