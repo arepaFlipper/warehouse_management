f1faf155d39bc022e06bfe86b1f354c3d302fc73
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-28 17:19:05 -0500

    🛰️ List popular products from the api

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,10 +1,16 @@
 "use client"
 
+import CardPopularProducts from "./CardPopularProducts"
+
 type Props = {}
 
 const Dashboard = (props: Props) => {
+  console.log(`💳%cpage.tsx:9 - process.env.NEXT_PUBLIC_API_BASE_URL`, 'font-weight:bold; background:#2fd000;color:#fff;'); //DELETEME:
+  console.log(process.env.NEXT_PUBLIC_API_BASE_URL); // DELETEME:
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
+      <CardPopularProducts />
       <div className="bg-gray-900 row-span-3 xl:row-span-6"></div>
       <div className="bg-gray-800 row-span-3 xl:row-span-6"></div>
       <div className="bg-gray-700 row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1"></div>
