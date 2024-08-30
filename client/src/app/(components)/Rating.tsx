714ea2263a1985e3c0d2fd76461d20273c34ad1e
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-08-30 11:10:28 -0500

    🚎 Add Rating component

---
+++ client/src/app/(components)/Rating.tsx
@@ -0,0 +1,21 @@
+import { Star } from "lucide-react";
+
+type RatingProps = {
+  rating: number | undefined;
+}
+
+const Rating = ({ rating }: RatingProps) => {
+  return (
+    <>
+      {
+        [1, 2, 3, 4, 5].map((index: number) => {
+          return (
+            <Star key={index} color={(rating && index <= rating) ? "#FFC107" : "#E4E5E9"} className="w-4 h-4" />
+          )
+        })
+      }
+    </>
+  )
+}
+
+export default Rating
