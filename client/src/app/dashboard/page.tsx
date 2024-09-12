ca31b7958f0ac00b9b2a2d1fe34ed2deea1c700a
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-12 17:18:37 -0500

    ➡️  add the CardSalesSummary calculations

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -7,9 +7,6 @@ import CardSalesSummary from "./CardSalesSummary"
 type Props = {}
 
 const Dashboard = (props: Props) => {
-  console.log(`💳%cpage.tsx:9 - process.env.NEXT_PUBLIC_API_BASE_URL`, 'font-weight:bold; background:#2fd000;color:#fff;'); //DELETEME:
-  console.log(process.env.NEXT_PUBLIC_API_BASE_URL); // DELETEME:
-
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows">
       <CardPopularProducts />
