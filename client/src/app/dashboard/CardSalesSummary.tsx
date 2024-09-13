97c655cfb51eeb59febcb3731969d076f0edc655
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-13 08:07:53 -0500

    🩳 fill Bars w/ linearGradient

--- client/src/app/dashboard/CardSalesSummary.tsx
+++ client/src/app/dashboard/CardSalesSummary.tsx
@@ -1,54 +1,136 @@
 import { useGetDashboardMetricsQuery } from "@/state/api";
 import { TrendingUp } from "lucide-react";
 import React, { useState } from "react";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+
 const CardSalesSummary = () => {
   const { data, isLoading, isError } = useGetDashboardMetricsQuery();
-  console.log(`📣 %cCardSalesSummary.tsx:6 - data`, 'font-weight:bold; background:#20df00;color:#fff;'); //DELETEME:
-  console.log(data); // DELETEME:
-  const salesData = data?.salesSummary || [];
+  const sales_data = data?.salesSummary || [];
 
   const [timeframe, setTimeframe] = useState("weekly");
 
-  const totalValueSum = salesData.reduce((acc, curr) => {
-    return acc + curr.totalValue
-  }, 0) || 0;
-  console.log(`🛣️%cCardSalesSummary.tsx:13 - totalValueSum`, 'font-weight:bold; background:#40bf00;color:#fff;'); //DELETEME:
-  console.log(totalValueSum); // DELETEME:
+  const totalValueSum =
+    sales_data.reduce((acc, curr) => acc + curr.totalValue, 0) || 0;
 
-  const averageChangePercentage = salesData.reduce((acc, curr, _, array) => {
-    return acc + curr.changePercentage! / array.length;
-  }, 0) || 0;
-  console.log(`👅%cCardSalesSummary.tsx:17 - averageChangePercentage`, 'font-weight:bold; background:#4fb000;color:#fff;'); //DELETEME:
-  console.log(averageChangePercentage); // DELETEME:
+  const averageChangePercentage =
+    sales_data.reduce((acc, curr, _, array) => {
+      return acc + curr.changePercentage! / array.length;
+    }, 0) || 0;
+
+  const highestValueData = sales_data.reduce((acc, curr) => {
+    return acc.totalValue > curr.totalValue ? acc : curr;
+  }, sales_data[0] || {});
+
+  const highestValueDate = highestValueData.date
+    ? new Date(highestValueData.date).toLocaleDateString("en-US", {
+      month: "numeric",
+      day: "numeric",
+      year: "2-digit",
+    })
+    : "N/A";
 
   if (isError) {
     return <div className="m-5">Failed to fetch data</div>;
   }
 
   return (
-    <div className="row-span-3 xl:row-span-5 bg-sky-50 shadow-md rounded-2xl flex flex-col justify-between">
+    <div className="row-span-3 xl:row-span-6 bg-white shadow-md rounded-2xl flex flex-col justify-between">
       {isLoading ? (
         <div className="m-5">Loading...</div>
       ) : (
         <>
-          <div><h2 className="text-lg font-semibold mb-2 px-7 pt-5">Sales Summary</h2></div>
-
-          {/* NOTE: BODY*/}
+          {/* HEADER */}
           <div>
-            {/* NOTE: BODY HEADER */}
-            <div className="flex justify-between items-center mb-6 px-7">
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Sales Summary
+            </h2>
+            <hr />
+          </div>
+
+          {/* BODY */}
+          <div>
+            {/* BODY HEADER */}
+            <div className="flex justify-between items-center mb-6 px-7 mt-5">
               <div className="text-lg font-medium">
                 <p className="text-xs text-gray-400">Value</p>
+                <span className="text-2xl font-extrabold">
+                  $
+                  {(totalValueSum / 1000000).toLocaleString("en", {
+                    maximumFractionDigits: 2,
+                  })}
+                  m
+                </span>
+                <span className="text-green-500 text-sm ml-2">
+                  <TrendingUp className="inline w-4 h-4 mr-1" />
+                  {averageChangePercentage.toFixed(2)}%
+                </span>
               </div>
-              <span className="text-2xl font-extrabold">
-                ${(totalValueSum / 1_000_000).toLocaleString("en", {
-                  maximumFractionDigits: 2,
-                })}m
-              </span>
-              <span className="text-green-500 text-sm ml-2">
-                <TrendingUp className="inline w-4 h-4 mr-1" />
-                {averageChangePercentage.toFixed(2)}%
-              </span>
+              <select
+                className="shadow-sm border border-gray-300 bg-white p-2 rounded"
+                value={timeframe}
+                onChange={(e) => {
+                  setTimeframe(e.target.value);
+                }}
+              >
+                <option value="daily">Daily</option>
+                <option value="weekly">Weekly</option>
+                <option value="monthly">Monthly</option>
+              </select>
+            </div>
+            {/* CHART */}
+            <ResponsiveContainer width="100%" height={350} className="px-7">
+              <BarChart data={sales_data} margin={{ top: 0, right: 0, left: -25, bottom: 0 }} >
+                <CartesianGrid strokeDasharray="" vertical={false} />
+                <XAxis dataKey="date" tickFormatter={(value) => {
+                  const date = new Date(value);
+                  return `${date.getMonth() + 1}/${date.getDate()}`;
+                }}
+                />
+                <YAxis
+                  tick={{ fontSize: 12, dx: -1 }} tickLine={false} axisLine={false}
+                  tickFormatter={(value) => {
+                    return `$${(value / 1_000_000).toFixed(0)}m`;
+                  }}
+                />
+                <Tooltip
+                  formatter={(value: number) => [`$${value.toLocaleString("en")}`]}
+                  labelFormatter={(label) => {
+                    const date = new Date(label);
+                    return date.toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    });
+                  }}
+                />
+                <defs>
+                  <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="100%" spreadMethod="reflect" >
+                    <stop offset="0" stopColor="#e66465" />
+                    <stop offset="1" stopColor="#9198e5" />
+                  </linearGradient>
+                </defs>
+                <Bar dataKey="totalValue" fill="url(#colorUv)" barSize={10} radius={[10, 10, 0, 0]} />
+              </BarChart>
+            </ResponsiveContainer>
+          </div>
+
+          {/* FOOTER */}
+          <div>
+            <hr />
+            <div className="flex justify-between items-center mt-6 text-sm px-7 mb-4">
+              <p>{sales_data.length || 0} days</p>
+              <p className="text-sm">
+                Highest Sales Date:{" "}
+                <span className="font-bold">{highestValueDate}</span>
+              </p>
             </div>
           </div>
         </>
