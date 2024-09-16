205473663ea2ccba7b97ad3571780b297112fa20
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-15 19:52:28 -0500

    🤤 add search bar to products route

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -1,10 +1,11 @@
 "use client"
 
 import { useGetProductsQuery } from "@/state/api";
+import { SearchIcon } from "lucide-react";
 import { useState } from "react";
 
 const Products = () => {
-  const [searchTerm, setSerachTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const { data: products, isLoading, isError } = useGetProductsQuery(searchTerm);
@@ -24,7 +25,15 @@ const Products = () => {
   }
 
   return (
-    <div className="mx-auto pb-5 w-full">Products</div>
+    <div className="mx-auto pb-5 w-full">
+      {/* NOTE: SEARCH BAR */}
+      <div className="mb-6">
+        <div className="flex items-center border-2 border-gray-200 rounded">
+          <SearchIcon className="w-5 h-5 text-gray-500 m-2" />
+          <input className="w-full py-2 px-4 rounded bg-white" placeholder="Search peoducts..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+        </div>
+      </div>
+    </div>
   )
 }
 
