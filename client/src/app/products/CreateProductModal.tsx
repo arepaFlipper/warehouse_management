707660bae9206d488c06add420f82d7d3376a17c
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-16 00:29:42 -0500

    🖌️ pop-up the CreateProductModal

--- client/src/app/products/CreateProductModal.tsx
+++ client/src/app/products/CreateProductModal.tsx
@@ -1,3 +1,7 @@
+import { ChangeEvent, FormEvent, useState } from "react";
+import { v4 } from "uuid";
+import Header from "../(components)/Header";
+
 type ProductFormData = {
   name: string;
   price: number;
@@ -12,8 +16,49 @@ type CreateProductModalProps = {
 };
 
 const CreateProductModal = ({ isOpen, onClose, onCreate }: CreateProductModalProps) => {
+  const [formData, setFormData] = useState({ productId: v4(), name: "", price: 0, stockQuantity: 0, rating: 0 });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onCreate(formData);
+    onClose();
+  }
+
+  if (!isOpen) {
+    return null;
+  }
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    setFormData({
+      ...formData,
+      [name]: (name === "price" || name === "stockQuantity" || name === "rating") ? parseFloat(value) : value,
+    });
+  }
+
+  const labelCssStyles = "block text-sm font-medium text-gray-700 ";
+  const inputCssStyles = "block w-full mb-2 p-2 border-gray-500 border-2 rounded-md ";
+
   return (
-    <div>CreateProductModal</div>
+    <div className="fixed inset-0 bg-reay-600 bg-opacity-50 overflow-y-auto h-full w-full z-20 ">
+      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white ">
+        <Header name="+ New Product" />
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="productName" className={labelCssStyles}>
+            Product Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+            value={formData.name}
+            className="block w-full mb-2 p-2 border-gray-500 border-2 rounded-md "
+            required
+          />
+        </form>
+      </div>
+    </div>
   )
 }
 
