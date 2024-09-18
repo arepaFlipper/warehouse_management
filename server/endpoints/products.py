d8f296280ced65e11c70d81f4b1c809d87a892d4
Cristian F. Tovar <cristian00tovar@gmail.com>
2024-09-18 12:50:46 -0500

    🪵 add scripts to test endpoints as client

---
+++ server/endpoints/products.py
@@ -0,0 +1,12 @@
+import requests
+
+# Define the URL
+url = "http://localhost:3002/products"
+
+# Send the GET request
+response = requests.get(url)
+
+# Print the response status code and content
+print(f"Status Code: {response.status_code}")
+print(f"Response Content: {response.text}")
+
